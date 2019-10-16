37376fc213d2eb9ccae24b2dfda5a14cf8eac2fb
William Barrie <williambarrie@dhcp-10-248-230-44.eduroam.wireless.private.cam.ac.uk>
2019-10-16 17:22:29 +0100

    test commit

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def print_names(names):
 
 
 print_names(['Joe', 'Gabe', 'Will', 'Cam'])
-print_names(['Elspeth', 'Chris', 'Tom'])
+
 
 for name in ["Joe", "Amy", "Brad", "Angelina", "Zuki", "Thandi", "Paris"]:
     print("Hi", name, "Please come to my party on Saturday!")

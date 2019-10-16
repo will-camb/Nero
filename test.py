660bc2cc247014832078b7ec648c988c76f1ad8d
William Barrie <williambarrie@dhcp-10-248-230-44.eduroam.wireless.private.cam.ac.uk>
2019-10-16 17:05:02 +0100

    test.py

--- test.py
+++ test.py
@@ -9,3 +9,4 @@ print_names(['Joe', 'Gabe', 'Will', 'Cam'])
 
 for name in ["Joe", "Amy", "Brad", "Angelina", "Zuki", "Thandi", "Paris"]:
     print("Hi", name, "Please come to my party on Saturday!")
+

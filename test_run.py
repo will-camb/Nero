e2dcce069ff364554252a7ecdba57be9768b4855
will-camb <williamfsbarrie@gmail.com>
2020-02-18 11:21:10 +0000

    Changed parameters for msprime model

--- test_run.py
+++ test_run.py
@@ -1,4 +1,4 @@
 from ms_output_to_fs_input import RunMsWithFsOutput
 
-model = RunMsWithFsOutput(nhaps=[2, 2, 2, 2, 2], length=1982950)
+model = RunMsWithFsOutput(nhaps=[8, 4, 4, 4, 4], length=1982950)
 model.run()

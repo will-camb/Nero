771b1267179d4b4fd8651395cfb8620861a0b275
will-camb <williamfsbarrie@gmail.com>
2020-11-26 17:34:06 +0000

    Changes to allow painting 1 by 1, with individual smaller phase files to reduce the compute time

---
+++ scripts/paintsample1by1.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo "Found $# arguments"
+if [ "$#" -lt "4" ] ; then
+    echo "Usage: paintsample1by1.sh <id> <name> <dir> <refdata.cp> <optional: removename>"
+    echo "<id>: the individual which is being painted against the reference panel"
+    echo "<n>: the number of the individual in the idfile"
+    echo "<phaselinenumber>: the line of the phasefile containing the individual's first haplotype"
+    echo "<cp_panel_scripts>: the location of the panel scripts which will be copied to each temp directory"
+    exit 0
+fi
+
+name="$1"
+number="$2"
+phaselinenumber="$3"
+cp_panel_scripts="$4"
+dir="temp.$name"
+chrlist=`seq 1 22`
+nhaps=638
+
+for chr in $chrlist; do
+  if [ ! -f $chr.master_all_copyprobsperlocus.txt ]; then
+    echo "Making $chr.master_all_copyprobsperlocus.txt"
+    touch $chr.master_all_copyprobsperlocus.txt
+fi
+
+mkdir -p
+echo "Using temporary directory $dir"
+mkdir -p "$dir"
+mkdir -p "$dir/phasefiles"
+cmd="cp $cp_panel_scripts/* $dir"
+echo "Copying scripts from $cp_panel_scripts"
+$cmd
+echo "Done copying scripts, now making new idfile at $dir"
+{ head -n $number ordered_all_pop_ids_mapped | tail -n 1 && tail -n 318 ordered_all_pop_ids_mapped; } > $dir/ordered_all_pop_ids_mapped
+
+#for chr in $chrlist ; do
+#  touch $dir/phasefiles/$chr.merged.phase
+#  head -n 3 phasefiles/$chr.merged.phase > $dir/phasefiles/$chr.merged.phase
+#  cat phasefiles/$chr.merged.phase | head -n $phaselinenumber | tail -n 2 >> $dir/phasefiles/$chr.merged.phase
+#  tail -n 636 phasefiles/$chr.merged.phase >> $dir/phasefiles/$chr.merged.phase
+#  sed -i ' ' "1s/.*/$nhaps/" $dir/phasefiles/$chr.merged.phase
+#done
+
+echo "Moving to directory $dir to run remaining commands"
+cd $dir
+cmd="bash will_04-paintvspanel.sh"
+echo $cmd
+$cmd
+wait
+cmd="bash will_modernvsancient/painting/commandlist1.txt"
+echo $cmd
+$cmd
+wait
+bash will_04-paintvspanel.sh
+
+for chr in $chrlist ; do
+  cat $dir/will_modernvsancient/painting/$chr.all_copyprobsperlocus.txt >> ../$chr.master_all_copyprobsperlocus.txt
+  rm $chr.all_copyprobsperlocus.txt
+  done
+rm -r $dir
--- scripts/will_3.5-paintvspanel1by1.py
+++ scripts/will_3.5-paintvspanel1by1.py
@@ -1,9 +1,20 @@
-#!/bin/sh
+import pandas as pd
+import argparse
 
-if [ "$#" -ne "1" ] ; then
-    echo "Usage: will_05-paintvspanel1by1.sh <idfile>"
-    echo "<idfile>: A file with the IDs of all individuals to be painted against the reference panel, 1 per row"
-    exit 0
-fi
+parser = argparse.ArgumentParser()
+parser.add_argument("-idfile",
+                    help="file containing all IDs to process; only those in pop 'UKBB' will be processed in current implementation",
+                    required=True)
+parser.add_argument("-cp_panel_scripts",
+                    help="the location of the panel scripts which will be copied to each temp directory",
+                    required=True)
+args = parser.parse_args()
+idfile = pd.read_csv(args.idfile, sep=" ", header=None)
+idfile = idfile[idfile[1]=='UKBB']
+idlist = idfile[0].tolist()
 
-idfile="$1"
+myfile = open('paintvspanel1by1_commands.txt', 'w')
+for n,id in enumerate(idlist):
+    myfile.write("bash paintsample1by1.sh " + str(id) + " " + str(n + 1) + " " + str((n+2)*2) + " " + args.cp_panel_scripts + "\n")
+myfile.close()
+

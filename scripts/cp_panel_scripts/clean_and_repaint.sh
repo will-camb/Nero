3fdd3635a29897ed4ccf8133331e09c750b0febe
will-camb <williamfsbarrie@gmail.com>
2020-12-07 20:26:03 +0000

    Minor format changes

--- scripts/cp_panel_scripts/clean_and_repaint.sh
+++ scripts/cp_panel_scripts/clean_and_repaint.sh
@@ -4,7 +4,7 @@ chrlist=`seq 1 22`
 for chr in $chrlist; do
     length=`head -n 1 $chr.master_all_copyprobsperlocus.txt`
     awk -F ',' -v nsnps="$length" 'length($3) == nsnps' $chr.master_all_copyprobsperlocus.txt > temp.$chr.master_all_copyprobsperlocus.txt
-    cut -f1 -d"," temp.$chr.master_all_copyprobsperlocus.txt | uniq -c > temp.{$chr}.counts
+    cut -f1 -d"," temp.$chr.master_all_copyprobsperlocus.txt | uniq -c > temp.$chr.counts
 done
 
 python3 << END
@@ -39,27 +39,28 @@ print("New painting commands written to repaint.paintvspanel1by1_commands.txt, n
 
 ## Now delete lines in results files containing the individuals to be repainted
 for i in range(1,23):
-  dummy=pd.read_csv("temp." + str(i) + ".master_all_copyprobsperlocus.txt",header=None, error_bad_lines=False)
-  dummy2 = dummy[~dummy[0].isin(to_paint)]
-  dummy2.to_csv("new." + str(i) + ".master_all_copyprobsperlocus.txt", header=False, index=False)
+  dummy=pd.read_csv("temp." + str(i) + ".master_all_copyprobsperlocus.txt", header=None, error_bad_lines=False)
+  dummy = dummy[~dummy[0].isin(to_paint)]
+  dummy.to_csv("new." + str(i) + ".master_all_copyprobsperlocus.txt", header=False, index=False)
 print("Done for master_all_copyprobsperlocus.txt files, now looking at chunkcounts etc")
-chunkcounts = pd.read_csv("ordered_all_pop_ids_mapped.allchr.chunkcounts.out", header=None, sep=" ", error_bad_lines=False)
+
+chunkcounts = pd.read_csv("ordered_all_pop_ids_mapped.allchr.chunkcounts.out", header=None, sep=" ", error_bad_lines=False, dtype = str)
 chunkcounts = chunkcounts[~chunkcounts[0].isin(to_paint)]
 chunkcounts.to_csv("new.ordered_all_pop_ids_mapped.allchr.chunkcounts.out", sep=" ", header=False, index=False)
 
-chunklength = pd.read_csv("ordered_all_pop_ids_mapped.allchr.chunklengths.out", header=None, sep=" ", error_bad_lines=False)
-chunklength = chunklength[~chunklength[0].isin(to_paint)]
-chunklength.to_csv("new.ordered_all_pop_ids_mapped.allchr.chunklength.out", sep=" ", header=False, index=False)
+chunklengths = pd.read_csv("ordered_all_pop_ids_mapped.allchr.chunklengths.out", header=None, sep=" ", error_bad_lines=False, dtype = str)
+chunklengths = chunklengths[~chunklengths[0].isin(to_paint)]
+chunklengths.to_csv("new.ordered_all_pop_ids_mapped.allchr.chunklengths.out", sep=" ", header=False, index=False)
 
-regionchunkcounts = pd.read_csv("ordered_all_pop_ids_mapped.allchr.regionchunkcounts.out", header=None, sep=" ", error_bad_lines=False)
+regionchunkcounts = pd.read_csv("ordered_all_pop_ids_mapped.allchr.regionchunkcounts.out", header=None, sep=" ", error_bad_lines=False, dtype = str)
 regionchunkcounts = regionchunkcounts[~regionchunkcounts[0].isin(to_paint)]
 regionchunkcounts.to_csv("new.ordered_all_pop_ids_mapped.allchr.regionchunkcounts.out", sep=" ", header=False, index=False)
 
-regionsquaredchunkcounts = pd.read_csv("ordered_all_pop_ids_mapped.allchr.regionsquaredchunkcounts.out", header=None, sep=" ", error_bad_lines=False)
+regionsquaredchunkcounts = pd.read_csv("ordered_all_pop_ids_mapped.allchr.regionsquaredchunkcounts.out", header=None, sep=" ", error_bad_lines=False, dtype = str)
 regionsquaredchunkcounts = regionsquaredchunkcounts[~regionsquaredchunkcounts[0].isin(to_paint)]
 regionsquaredchunkcounts.to_csv("new.ordered_all_pop_ids_mapped.allchr.regionsquaredchunkcounts.out", sep=" ", header=False, index=False)
 
-mutationprobs = pd.read_csv("ordered_all_pop_ids_mapped.allchr.mutationprobs.out", header=None, sep=" ", error_bad_lines=False)
+mutationprobs = pd.read_csv("ordered_all_pop_ids_mapped.allchr.mutationprobs.out", header=None, sep=" ", error_bad_lines=False, dtype = str)
 mutationprobs = mutationprobs[~mutationprobs[0].isin(to_paint)]
 mutationprobs.to_csv("new.ordered_all_pop_ids_mapped.allchr.mutationprobs.out", sep=" ", header=False, index=False)
 END

3d2df2bf1e06d863467adfe015b74a9fc52e226a
will-camb <williamfsbarrie@gmail.com>
2021-01-20 15:41:44 +0000

    Now weights SNPs by their relative distance to other SNPs

--- scripts/PRS_calculator.py
+++ scripts/PRS_calculator.py
@@ -11,19 +11,17 @@ def analyse_anc(anc_copyprobs, anc):
     for i in list_of_SNPs:
         anc_copyprobs_i = pd.DataFrame(zip(anc_copyprobs.index, haps, anc_copyprobs[i]))
         phase_i = pd.DataFrame(zip(phase.index, haps, phase[i]))
-        df = pd.merge(anc_copyprobs_i, phase_i, left_on=[0,1], right_on=[0,1]).set_index(0)
-        df.columns=['haps', str(anc), 'phase']
-        df1 = df.loc[df[anc]>=6]
+        df = pd.merge(anc_copyprobs_i, phase_i, left_on=[0, 1], right_on=[0, 1]).set_index(0)
+        df.columns = ['haps', str(anc), 'phase']
+        df1 = df.loc[df[anc] >= 6]
         if df1.shape[0] > 1:
-            MAF = df1.loc[df1['phase']=='0'].shape[0]/df1.shape[0]
-            Beta = GWAS_n.loc[GWAS_n['pos']==i].beta.item()
-            anc_dict[anc].append([MAF,Beta])
+            MAF = df1.loc[df1['phase'] == '0'].shape[0] / df1.shape[0]
+            Beta = GWAS_n.loc[GWAS_n['pos'] == i].beta.item()
+            vector = vectors[vectors['start.pos'].astype(int) == i]['vector'].item()
+            anc_dict[anc].append([MAF, Beta, vector])
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-copyprobs_directory",
-                    help="Directory of all_copyprobsperlocus files; should be named in form n.all_copyprobsperlocus.txt.gz",
-                    required=True)
 parser.add_argument("-phasefile_directory",
                     help="Directory of phasefiles; should be named in form chr#.merged.phase",
                     required=True)
@@ -31,7 +29,8 @@ parser.add_argument("-idfile",
                     help="Directory of idfile used in chromopainter ('individual pop 1')",
                     required=True)
 parser.add_argument("-file_name",
-                    help="Phenotype being looked at; this should be the file name in the Neale Lab google sheet; NB extension must be .gz and not.bgz!",
+                    help="Phenotype being looked at; this should be the file name in the Neale Lab google sheet; NB "
+                         "extension must be .gz and not.bgz!",
                     required=True)
 args = parser.parse_args()
 # Load Neale Lab effect size for this phenotype
@@ -44,10 +43,21 @@ idfile = idfile[~idfile.index.isin(idfile.tail(318).index.tolist())]
 anc_dict = defaultdict(list)
 for n in range(1, 23):
     print("Processing chromosome" + str(n))
-    copyprobs = pd.read_csv(str(args.copyprobs_directory) + "/" + str(n) + ".master_all_copyprobsperlocus.txt.gz", header=None,
-                            skiprows=1, index_col=0)
-    positions = pd.read_csv(str(args.phasefile_directory) + "/" + str(n) + ".merged.phase", skiprows=2, nrows=1,
-                            sep=" ", header=None).T.drop(0)
+    positions = pd.read_csv(str(args.phasefile_directory) + "/" + str(n) + ".merged.phase",
+                            skiprows=2,
+                            nrows=1,
+                            sep=" ",
+                            header=None).T.drop(0)
+    vectors = pd.read_csv("17.merged.phase", skiprows=2, nrows=1, sep=" ", header=None).T.drop(0)
+    vectors.columns = ['start.pos']
+    vectors['previous'] = vectors['start.pos'].shift(1)
+    vectors['next'] = vectors['start.pos'].shift(-1)
+    vectors = vectors.fillna(0)
+    vectors['vector'] = (vectors.next - vectors.previous) / 2
+    vectors.loc[vectors.index[-1], 'vector'] = 0
+    vectors.loc[vectors.index[-1], 'vector'] = vectors.vector.mean()
+    vectors = vectors[['start.pos', 'vector']]
+    vectors['start.pos'] = vectors['start.pos'].astype(str)
     GWAS_n = GWAS[GWAS['chr'] == str(n)]
     GWAS_n = GWAS_n.loc[GWAS_n['pos'].astype(str).isin(positions[0].astype(str).tolist())]
     GWAS_n = GWAS_n[GWAS_n['pval'] <= 0.05]
@@ -63,24 +73,35 @@ for n in range(1, 23):
         haps.extend([1, 2])
     for anc in ["CHG", "EHG", "Farmer", "WHG", "Yamnaya"]:
         if anc == 'CHG':
-            analyse_anc(copyprobs.iloc[0::10], anc)
+            print("calculating PRS for CHG, chr" + str(n))
+            analyse_anc(pd.read_csv("temp.CHG." + str(n) + ".master_all_copyprobsperlocus.txt",
+                            header=None,
+                            index_col=0), anc)
         elif anc == 'EHG':
-            analyse_anc(copyprobs.iloc[1::10], anc)
+            print("calculating PRS for EHG, chr" + str(n))
+            analyse_anc(pd.read_csv("temp.EHG." + str(n) + ".master_all_copyprobsperlocus.txt",
+                                    header=None,
+                                    index_col=0), anc)
         elif anc == 'Farmer':
-            anc_copyprobs = copyprobs.iloc[5::10][2].dropna().apply(lambda x: pd.Series(list(x))).apply(pd.to_numeric) + \
-                            copyprobs.iloc[4::10][2].dropna().apply(lambda x: pd.Series(list(x))).apply(pd.to_numeric) + \
-                            copyprobs.iloc[2::10][2].dropna().apply(lambda x: pd.Series(list(x))).apply(pd.to_numeric) + \
-                            copyprobs.iloc[3::10][2].dropna().apply(lambda x: pd.Series(list(x))).apply(pd.to_numeric)
-            analyse_anc(anc_copyprobs, anc)
+            print("calculating PRS for Farmer, chr" + str(n))
+            analyse_anc(pd.read_csv("temp.Farmer." + str(n) + ".master_all_copyprobsperlocus.txt",
+                                    header=None, sep=" ",
+                                    index_col=0), anc)
         elif anc == 'WHG':
-            analyse_anc(copyprobs.iloc[8::10], anc)
+            print("calculating PRS for WHG, chr" + str(n))
+            analyse_anc(pd.read_csv("temp.WHG." + str(n) + ".master_all_copyprobsperlocus.txt",
+                                    header=None,
+                                    index_col=0), anc)
         elif anc == 'Yamnaya':
-            analyse_anc(copyprobs.iloc[9::10], anc)
+            print("calculating PRS for Yamnaya, chr" + str(n))
+            analyse_anc(pd.read_csv("temp.Yamnaya." + str(n) + ".master_all_copyprobsperlocus.txt",
+                                    header=None,
+                                    index_col=0), anc)
 PRS_dict = dict()
 for anc in ["CHG", "EHG", "Farmer", "WHG", "Yamnaya"]:
     PRS = 0
-    for j, k in anc_dict[anc]:
-        PRS += j * k
+    for j, k, l in anc_dict[anc]:
+        PRS += j * k * l
     PRS_dict[anc] = PRS
 PRS_dict['Phenotype'] = args.file_name
 PRS_calculations_temp = pd.DataFrame([PRS_dict])

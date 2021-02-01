2fb6b4469147de2771a33840844dc2e130a7a571
will-camb <williamfsbarrie@gmail.com>
2021-02-01 10:58:13 +0000

    Works for list of phenotypes, calculates MAF*Beta*vector

--- scripts/PRS_calculator.py
+++ scripts/PRS_calculator.py
@@ -4,20 +4,22 @@ import os
 from collections import defaultdict
 
 
-def analyse_anc(anc_copyprobs, anc):
+def analyse_anc(anc_copyprobs, anc, chrom):
+    global PRS
+    anc_copyprobs = anc_copyprobs[[str(a) for a in list_of_SNPs]]
+    PRS = 0
     for i in list_of_SNPs:
         anc_copyprobs_i = pd.DataFrame(zip(anc_copyprobs.index, haps, anc_copyprobs[str(i)]))
         phase_i = pd.DataFrame(zip(phase.index, haps, phase[i]))
         df = pd.merge(anc_copyprobs_i, phase_i, left_on=[0, 1], right_on=[0, 1]).set_index(0)
         df.columns = ['haps', str(anc), 'phase']
-        df1 = df.loc[df[anc] >= 6]
-        if df1.shape[0] > 1:
-            minor = df.loc[df['phase'] == '0'][anc].sum() #Sum painting prob for minor allele
-            major = df.loc[df['phase'] == '1'][anc].sum() #Sum painting prob for major allele
-            anc_maf = minor / (minor + major)
-            Beta = GWAS_n.loc[GWAS_n['pos'] == i].beta.item()
-            vector = vectors[vectors['start.pos'].astype(int) == i]['vector'].item()
-            anc_dict[anc].append([anc_maf, Beta, vector])
+        minor = df.loc[df['phase'] == '0'][anc].sum() #Sum painting prob for minor allele
+        major = df.loc[df['phase'] == '1'][anc].sum() #Sum painting prob for major allele
+        anc_maf = minor / (minor + major + 1)
+        Beta = GWAS_n.loc[GWAS_n['pos'] == i].beta.item()
+        vector = vectors[vectors['start.pos'].astype(int) == i]['vector'].item()
+        PRS += anc_maf * Beta * vector
+    anc_dict[anc].append([chrom, PRS])
 
 
 parser = argparse.ArgumentParser()
@@ -35,14 +37,16 @@ args = parser.parse_args()
 # Load Neale Lab effect size for this phenotype
 GWAS = pd.read_csv(args.file_name, sep='\t')
 GWAS[['chr', 'pos', '1', '2']] = GWAS['variant'].str.split(':', expand=True)
+GWAS = GWAS[GWAS['pval'] <= 0.05]
 if not os.path.exists("PRS_calculations"):
     open("PRS_calculations", 'a').close()
 idfile = pd.read_csv(args.idfile, header=None, sep=" ", index_col=0)
 idfile = idfile[~idfile.index.isin(idfile.tail(318).index.tolist())]
 anc_dict = defaultdict(list)
-print("DONE GWAS LOAD")
+PRS = 0
+print("Done loading of GWAS results for " + str(args.file_name))
 for n in range(1, 23):
-    print("Processing chromosome" + str(n))
+    print("Processing chromosome " + str(n))
     positions = pd.read_csv(str(args.phasefile_directory) + "/" + str(n) + ".merged.phase",
                             skiprows=2,
                             nrows=1,
@@ -64,7 +68,6 @@ for n in range(1, 23):
     vectors['start.pos'] = vectors['start.pos'].astype(str)
     GWAS_n = GWAS[GWAS['chr'] == str(n)]
     GWAS_n = GWAS_n.loc[GWAS_n['pos'].astype(str).isin(positions[0].astype(str).tolist())]
-    GWAS_n = GWAS_n[GWAS_n['pval'] <= 0.05]
     GWAS_n.pos = pd.to_numeric(GWAS_n.pos)
     list_of_SNPs = GWAS_n['pos'].tolist()
     phase = pd.read_csv(str(args.phasefile_directory) + "/" + str(n) + ".merged.phase", skiprows=3, header=None)
@@ -76,40 +79,53 @@ for n in range(1, 23):
     haps = list()
     for h in range(int(phase.shape[0] / 2)):
         haps.extend([1, 2])
-    for anc in ["CHG", "EHG", "Farmer", "WHG", "Yamnaya"]:
+    for anc in ["CHG", "EHG", "Farmer", "African", "EastAsian", "WHG", "Yamnaya"]:
         if anc == 'CHG':
-            print("Calculating PRS for CHG, chr" + str(n))
-            anc_copyprobs = pd.read_csv("temp.CHG." + str(n) + ".master_all_copyprobsperlocus.txt", sep=" ", index_col=0)
-            anc_copyprobs = anc_copyprobs[[str(a) for a in list_of_SNPs]]
-            analyse_anc(anc_copyprobs, anc)
+            print("Calculating MAF/Beta/vectors for CHG, chr" + str(n))
+            anc_copyprobs = pd.read_csv("temp.CHG." + str(n) + ".master_all_copyprobsperlocus.txt.gz", sep=" ", index_col=0)
+            analyse_anc(anc_copyprobs, anc, n)
         elif anc == 'EHG':
-            print("Calculating PRS for EHG, chr" + str(n))
-            anc_copyprobs = pd.read_csv("temp.EHG." + str(n) + ".master_all_copyprobsperlocus.txt", sep=" ", index_col=0)
-            anc_copyprobs = anc_copyprobs[[str(a) for a in list_of_SNPs]]
-            analyse_anc(anc_copyprobs, anc)
+            print("Calculating MAF/Beta/vectors for EHG, chr" + str(n))
+            anc_copyprobs = pd.read_csv("temp.EHG." + str(n) + ".master_all_copyprobsperlocus.txt.gz", sep=" ", index_col=0)
+            analyse_anc(anc_copyprobs, anc, n)
         elif anc == 'Farmer':
-            print("Calculating PRS for Farmer, chr" + str(n))
-            anc_copyprobs = pd.read_csv("temp.Farmer." + str(n) + ".master_all_copyprobsperlocus.txt", sep=" ", index_col=0)
-            anc_copyprobs = anc_copyprobs[[str(a) for a in list_of_SNPs]]
-            analyse_anc(anc_copyprobs, anc)
+            print("Calculating MAF/Beta/vectors for Farmer, chr" + str(n))
+            anc_copyprobs = pd.read_csv("temp.Farmer." + str(n) + ".master_all_copyprobsperlocus.txt.gz", sep=" ", index_col=0)
+            analyse_anc(anc_copyprobs, anc, n)
+        elif anc == 'African':
+            print("Calculating MAF/Beta/vectors for African, chr" + str(n))
+            anc_copyprobs = pd.read_csv("temp.African." + str(n) + ".master_all_copyprobsperlocus.txt.gz", sep=" ",
+                                        index_col=0)
+            analyse_anc(anc_copyprobs, anc, n)
+        elif anc == 'EastAsian':
+            print("Calculating MAF/Beta/vectors for EastAsian, chr" + str(n))
+            anc_copyprobs = pd.read_csv("temp.EastAsian." + str(n) + ".master_all_copyprobsperlocus.txt.gz", sep=" ",
+                                        index_col=0)
+            analyse_anc(anc_copyprobs, anc, n)
         elif anc == 'WHG':
-            print("Calculating PRS for WHG, chr" + str(n))
-            anc_copyprobs = pd.read_csv("temp.WHG." + str(n) + ".master_all_copyprobsperlocus.txt", sep=" ", index_col=0)
-            anc_copyprobs = anc_copyprobs[[str(a) for a in list_of_SNPs]]
-            analyse_anc(anc_copyprobs, anc)
+            print("Calculating MAF/Beta/vectors for WHG, chr" + str(n))
+            anc_copyprobs = pd.read_csv("temp.WHG." + str(n) + ".master_all_copyprobsperlocus.txt.gz", sep=" ", index_col=0)
+            analyse_anc(anc_copyprobs, anc, n)
         elif anc == 'Yamnaya':
-            print("Calculating PRS for Yamnaya, chr" + str(n))
-            anc_copyprobs = pd.read_csv("temp.Yamnaya." + str(n) + ".master_all_copyprobsperlocus.txt", sep=" ", index_col=0)
-            anc_copyprobs = anc_copyprobs[[str(a) for a in list_of_SNPs]]
-            analyse_anc(anc_copyprobs, anc)
-PRS_dict = dict()
+            print("Calculating MAF/Beta/vectors for Yamnaya, chr" + str(n))
+            anc_copyprobs = pd.read_csv("temp.Yamnaya." + str(n) + ".master_all_copyprobsperlocus.txt.gz", sep=" ", index_col=0)
+            analyse_anc(anc_copyprobs, anc, n)
 print("*** Success! Now calculating PRS per anc and writing to file ***")
-for anc in ["CHG", "EHG", "Farmer", "WHG", "Yamnaya"]:
-    PRS = 0
-    for j, k, l in anc_dict[anc]:
-        PRS += j * k * l
-    PRS_dict[anc] = PRS
+# PRS_dict = dict()
+# for anc in ["CHG", "EHG", "Farmer", "African", "EastAsian", "WHG", "Yamnaya"]:
+#     PRS = 0
+#     for j, k, l in anc_dict[anc]:
+#         PRS += j * k * l
+#     PRS_dict[anc] = PRS
+PRS_dict = dict()
+for anc in ["CHG", "EHG", "Farmer", "African", "EastAsian", "WHG", "Yamnaya"]:
+    PRS_per_anc = 0
+    for chrom, PRS in anc_dict[anc]:
+        PRS_per_anc += PRS
+    PRS_dict[anc] = PRS_per_anc
+
 PRS_dict['Phenotype'] = args.file_name
 PRS_calculations_temp = pd.DataFrame([PRS_dict])
-PRS_calculations_temp = PRS_calculations_temp[["Phenotype", "CHG", "EHG", "Farmer", "WHG", "Yamnaya"]]
+PRS_calculations_temp = PRS_calculations_temp[["Phenotype", "CHG", "EHG", "Farmer", "African", "EastAsian", "WHG", "Yamnaya"]]
 PRS_calculations_temp.to_csv('PRS_calculations', mode='a', index=False, header=False, sep=" ")
+print("*** Success! See PRS_calculations for results ***")
--- scripts/run_PRS_calculator.sh
+++ scripts/run_PRS_calculator.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #cd $PBS_O_WORKDIR
 module load anaconda3/4.4.0
+#source /willerslev/software/venv_python3.6/bin/activate
 
 if [ "$#" -ne "5" ] ; then
     echo "Usage: run_PRS_calculator.sh <manifest> <file_name> <copyprobs_directory> <phasefile_directory>"
     echo "<manifest>: the manifest csv file"
-    echo "<file_name>: filename from manifest to be calculated"
+    echo "<phenotype_file>: file containing filename from manifest to be calculated, 1 per row eg E4_OBESITY.gwas.imputed_v3.both_sexes.tsv.bgz"
     echo "<copyprobs_directory>: Directory of all_copyprobsperlocus files; should be named in form n.master_all_copyprobsperlocus.txt.gz"
     echo "<phasefile_directory>: Directory of phasefiles; should be named in form chr#.merged.phase"
     echo "<idfile>: Path to idfile: ordered_all_pop_ids_mapped ('individualID popID 1')"
@@ -13,42 +14,66 @@ if [ "$#" -ne "5" ] ; then
 fi
 
 manifest="$1"
-file_name="$2"
+phenotype_file="$2"
+#file_name=$2
 copyprobs_directory="$3"
 phasefile_directory="$4"
 idfile="$5"
+
+#Loop through lines and download results
+#while IFS= read -r line; do
+#echo "Extracting results from Dropbox for $line"
+#`awk -F ',' -v $line="$line" '$5 == line { print $6 }' $manifest`
+#MY_FILE=$line
+#NEW_EXT=${MY_FILE/bgz/gz}
+#mv $line "$NEW_EXT"
+#done < $phenotype_file
+
 echo "*** Extracting results file from Dropbox ***"
 #`awk -F ',' -v file_name="$file_name" '$5 == file_name { print $6 }' $manifest`
-MY_FILE=$file_name
-NEW_EXT=${MY_FILE/bgz/gz}
+#MY_FILE=$file_name
+#NEW_EXT=${MY_FILE/bgz/gz}
 #mv $file_name "$NEW_EXT"
 echo "*** Success! Now making temp copyprobs files per ancestry ***"
-chrlist=`seq 1 22`
-for chr in $chrlist; do
-awk 'NR % 10 == 1' $3/$chr.master_all_copyprobsperlocus.txt > temp.CHG.$chr.master_all_copyprobsperlocus.txt
-awk 'NR % 10 == 2' $3/$chr.master_all_copyprobsperlocus.txt > temp.EHG.$chr.master_all_copyprobsperlocus.txt
-awk 'NR % 10 == 3' $3/$chr.master_all_copyprobsperlocus.txt > temp.FarmerAnatolian.$chr.master_all_copyprobsperlocus.txt
-awk 'NR % 10 == 4' $3/$chr.master_all_copyprobsperlocus.txt > temp.FarmerEarly.$chr.master_all_copyprobsperlocus.txt
-awk 'NR % 10 == 5' $3/$chr.master_all_copyprobsperlocus.txt > temp.FarmerLate.$chr.master_all_copyprobsperlocus.txt
-awk 'NR % 10 == 6' $3/$chr.master_all_copyprobsperlocus.txt > temp.FarmerMiddle.$chr.master_all_copyprobsperlocus.txt
-awk 'NR % 10 == 7' $3/$chr.master_all_copyprobsperlocus.txt > temp.EastAsian.$chr.master_all_copyprobsperlocus.txt
-awk 'NR % 10 == 8' $3/$chr.master_all_copyprobsperlocus.txt > temp.African.$chr.master_all_copyprobsperlocus.txt
-awk 'NR % 10 == 9' $3/$chr.master_all_copyprobsperlocus.txt > temp.WHG.$chr.master_all_copyprobsperlocus.txt
-awk 'NR % 10 == 0' $3/$chr.master_all_copyprobsperlocus.txt > temp.Yamnaya.$chr.master_all_copyprobsperlocus.txt
-done
+#chrlist=`seq 1 22`
+chrlist=`seq 1 21`
+#for chr in $chrlist; do
+#zcat $3/$chr.master_all_copyprobsperlocus.txt.gz | awk 'NR % 10 == 1' - > temp.CHG.$chr.master_all_copyprobsperlocus.txt
+#gzip temp.CHG.$chr.master_all_copyprobsperlocus.txt
+#zcat $3/$chr.master_all_copyprobsperlocus.txt.gz | awk 'NR % 10 == 2' - > temp.EHG.$chr.master_all_copyprobsperlocus.txt
+#gzip temp.EHG.$chr.master_all_copyprobsperlocus.txt
+#zcat $3/$chr.master_all_copyprobsperlocus.txt.gz | awk 'NR % 10 == 3' - > temp.FarmerAnatolian.$chr.master_all_copyprobsperlocus.txt
+#gzip temp.FarmerAnatolian.$chr.master_all_copyprobsperlocus.txt
+#zcat $3/$chr.master_all_copyprobsperlocus.txt.gz | awk 'NR % 10 == 4' - > temp.FarmerEarly.$chr.master_all_copyprobsperlocus.txt
+#gzip temp.FarmerEarly.$chr.master_all_copyprobsperlocus.txt
+#zcat $3/$chr.master_all_copyprobsperlocus.txt.gz | awk 'NR % 10 == 5' - > temp.FarmerLate.$chr.master_all_copyprobsperlocus.txt
+#gzip temp.FarmerLate.$chr.master_all_copyprobsperlocus.txt
+#zcat $3/$chr.master_all_copyprobsperlocus.txt.gz | awk 'NR % 10 == 6' - > temp.FarmerMiddle.$chr.master_all_copyprobsperlocus.txt
+#gzip temp.FarmerMiddle.$chr.master_all_copyprobsperlocus.txt
+#zcat $3/$chr.master_all_copyprobsperlocus.txt.gz | awk 'NR % 10 == 7' - > temp.EastAsian.$chr.master_all_copyprobsperlocus.txt
+#gzip temp.EastAsian.$chr.master_all_copyprobsperlocus.txt
+#zcat $3/$chr.master_all_copyprobsperlocus.txt.gz | awk 'NR % 10 == 8' - > temp.African.$chr.master_all_copyprobsperlocus.txt
+#gzip temp.African.$chr.master_all_copyprobsperlocus.txt
+#zcat $3/$chr.master_all_copyprobsperlocus.txt.gz | awk 'NR % 10 == 9' - > temp.WHG.$chr.master_all_copyprobsperlocus.txt
+#gzip temp.WHG.$chr.master_all_copyprobsperlocus.txt
+#zcat $3/$chr.master_all_copyprobsperlocus.txt.gz | awk 'NR % 10 == 0' - > temp.Yamnaya.$chr.master_all_copyprobsperlocus.txt
+#gzip temp.Yamnaya.$chr.master_all_copyprobsperlocus.txt
+#done
+echo "*** Success! Now making farmer aggregate, and reformatting temp files in Python ***"
 python3 << END
 import pandas as pd
 for i in range(1,23):
-  copyprobsFA = pd.read_csv("temp.FarmerAnatolian."+str(i)+".master_all_copyprobsperlocus.txt",
+  print("Reading Farmer chr" + str(i))
+  copyprobsFA = pd.read_csv("temp.FarmerAnatolian."+str(i)+".master_all_copyprobsperlocus.txt.gz",
                               header=None,
                               index_col=0)
-  copyprobsFE = pd.read_csv("temp.FarmerEarly."+str(i)+".master_all_copyprobsperlocus.txt",
+  copyprobsFE = pd.read_csv("temp.FarmerEarly."+str(i)+".master_all_copyprobsperlocus.txt.gz",
                               header=None,
                               index_col=0)
-  copyprobsFL = pd.read_csv("temp.FarmerLate."+str(i)+".master_all_copyprobsperlocus.txt",
+  copyprobsFL = pd.read_csv("temp.FarmerLate."+str(i)+".master_all_copyprobsperlocus.txt.gz",
                               header=None,
                               index_col=0)
-  copyprobsFM = pd.read_csv("temp.FarmerMiddle."+str(i)+".master_all_copyprobsperlocus.txt",
+  copyprobsFM = pd.read_csv("temp.FarmerMiddle."+str(i)+".master_all_copyprobsperlocus.txt.gz",
                               header=None,
                               index_col=0)
   copyprobsFarmer = copyprobsFA[2].dropna().apply(lambda x: pd.Series(list(x))).apply(pd.to_numeric) + \
@@ -56,12 +81,15 @@ for i in range(1,23):
                               copyprobsFL[2].dropna().apply(lambda x: pd.Series(list(x))).apply(pd.to_numeric) + \
                               copyprobsFM[2].dropna().apply(lambda x: pd.Series(list(x))).apply(pd.to_numeric)
   copyprobsFarmer.to_csv("temp.Farmer."+str(i)+".master_all_copyprobsperlocus.txt", header=False)
-  for anc in ["CHG", "EHG", "Farmer", "WHG", "Yamnaya"]:
+  print("Farmer done, now converting other ancestries to correct format")
+  for anc in ["CHG", "EHG", "Farmer", "African", "EastAsian", "WHG", "Yamnaya"]:
     print("Processing " + str(anc) + " chromosome " + str(i))
-    anc_copyprobs = pd.read_csv("temp." + str(anc) + "." + str(i) + ".master_all_copyprobsperlocus.txt", header=None, index_col=0)
-    if anc != 'Farmer':
+    if anc == 'Farmer':
+      anc_copyprobs = pd.read_csv("temp." + str(anc) + "." + str(i) + ".master_all_copyprobsperlocus.txt", header=None, index_col=0)
+    else:
+      anc_copyprobs = pd.read_csv("temp." + str(anc) + "." + str(i) + ".master_all_copyprobsperlocus.txt.gz", header=None, index_col=0)
       anc_copyprobs = anc_copyprobs[2].dropna().apply(lambda x: pd.Series(list(x))).apply(pd.to_numeric)
-    positions = pd.read_csv("$phasefile_directory" + str(i) + ".merged.phase",
+    positions = pd.read_csv("$phasefile_directory/" + str(i) + ".merged.phase",
                             skiprows=2,
                             nrows=1,
                             sep=" ",
@@ -70,9 +98,31 @@ for i in range(1,23):
     anc_copyprobs.to_csv("temp." + str(anc) + "." + str(i) + ".master_all_copyprobsperlocus.txt", sep=" ")
     print(str(anc) + " done!")
 END
+echo "*** Success! Now gzipping temp files ***"
+for chr in $chrlist; do
+gzip --force temp.CHG.$chr.master_all_copyprobsperlocus.txt
+gzip --force temp.EHG.$chr.master_all_copyprobsperlocus.txt
+gzip --force temp.Farmer.$chr.master_all_copyprobsperlocus.txt
+gzip --force temp.EastAsian.$chr.master_all_copyprobsperlocus.txt
+gzip --force temp.African.$chr.master_all_copyprobsperlocus.txt
+gzip --force temp.WHG.$chr.master_all_copyprobsperlocus.txt
+gzip --force temp.Yamnaya.$chr.master_all_copyprobsperlocus.txt
+done
 echo "*** Success! Now running PRS calculator: ***"
-echo "python3 PRS_calculator.py -phasefile_directory $phasefile_directory -idfile $idfile -file_name $NEW_EXT"
-`python3 PRS_calculator.py -phasefile_directory $phasefile_directory -idfile $idfile -file_name $NEW_EXT`
+#while IFS= read -r line; do
+#echo "Writing PRS_calculator commands for $line"
+##MY_FILE=$line
+##NEW_EXT=${MY_FILE/bgz/gz}
+#NEW_EXT=$line
+#echo "python3 PRS_calculator.py -phasefile_directory $phasefile_directory -idfile $idfile -file_name $NEW_EXT" >> PRS_calculator_commands
+#done < "$phenotype_file"
+
+echo "Now running commands in PRS_calculator_commands in parallel"
+#cat PRS_calculator_commands | parallel
+
+#echo "python3 PRS_calculator.py -phasefile_directory $phasefile_directory -idfile $idfile -file_name $NEW_EXT"
+#`python3 PRS_calculator.py -phasefile_directory $phasefile_directory -idfile $idfile -file_name $NEW_EXT`
 echo "*** All done, now tidying up temp files ***"
 #rm $NEW_EXT
 #rm temp*
+#rm PRS_calculator_commands

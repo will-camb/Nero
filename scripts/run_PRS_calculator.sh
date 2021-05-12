619403f6db97061585dbe2656c499f4e3172d2a5
will-camb <williamfsbarrie@gmail.com>
2021-05-12 09:08:02 +0100

    Now shuffles commands before running so don't run all the largest files first (40 nodes)

--- scripts/run_PRS_calculator.sh
+++ scripts/run_PRS_calculator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #cd $PBS_O_WORKDIR
 #module load anaconda3/4.4.0
-#source /willerslev/software/venv_python3.6/bin/activate
+source /willerslev/software/venv_python3.6/bin/activate
 
 if [ "$#" -ne "4" ] ; then
     echo "Usage: run_PRS_calculator.sh <phenotype_file> <copyprobs_directory> <phasefile_directory> <idfile>"
@@ -22,12 +22,14 @@ ancestries="CHG EHG Farmer African EastAsian WHG Yamnaya"
 
 for chr in $chrlist; do
   for anc in $ancestries; do
-    echo "python3 PRS_calculator.py -copyprobs_file $copyprobs_directory/temp.$anc.$chr.master_all_copyprobsperlocus.txt.gz -phasefile $phasefile_directory/transformed.$chr.merged.phase.gz -idfile $idfile -phenotypes $phenotype_file -pruned_dir $pruned_dir -chr $chr -anc $anc" >> PRS_calculator_commands
+    echo "python3 PRS_calculator.py -copyprobs_file $copyprobs_directory/temp.$anc.$chr.master_all_copyprobsperlocus.txt.gz -phasefile $phasefile_directory/transformed.$chr.merged.phase.gz -idfile $idfile -phenotypes $phenotype_file -pruned_dir $pruned_dir -chr $chr -anc $anc" >> PRS_calculator_commands_temp
   done
 done
 
 echo "Now running commands in PRS_calculator_commands in parallel"
-cat PRS_calculator_commands | parallel  -j 60
+shuf PRS_calculator_commands_temp > PRS_calculator_commands
+rm PRS_calculator_commands_temp
+cat PRS_calculator_commands | parallel  -j 40
 
 echo "*** All done, results are in PRS_calculations ***"
 

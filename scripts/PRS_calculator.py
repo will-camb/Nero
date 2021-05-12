f6cd82e0a22a99c39484d1ba2dd602508c9d93f3
will-camb <williamfsbarrie@gmail.com>
2021-05-12 09:09:07 +0100

    Checks which variant (major/mior) is the effect allele in variants.tsv.gz and chooses the correct coding from the phasefiles to use

--- scripts/PRS_calculator.py
+++ scripts/PRS_calculator.py
@@ -2,10 +2,11 @@ import pandas as pd
 import argparse
 import os
 
-# Could print warning when maf is > 0.5?
 
 def analyse_anc(anc_copyprobs_temp, anc, chrom, pval, LD_value, phenotype_file):
     global list_of_SNPs
+    global alt
+    global ref
     PRS = 0
     skipped_snps = 0
     for i in list_of_SNPs:
@@ -13,26 +14,40 @@ def analyse_anc(anc_copyprobs_temp, anc, chrom, pval, LD_value, phenotype_file):
         phase_i = pd.DataFrame(zip(phase_temp.index, haps, phase_temp[str(i)]))
         df = pd.merge(anc_copyprobs_temp_i, phase_i, left_on=[0, 1], right_on=[0, 1]).set_index(0)
         df.columns = ['haps', anc, 'phase']
-        minor_df = df.loc[df['phase'] == 0]
-        if minor_df.empty:
-            minor = 0
+
+        #  Check if the minor allele = alt allele
+        if GWAS_pruned.loc[GWAS_pruned['pos'] == i].minor_allele.item() == \
+                GWAS_pruned.loc[GWAS_pruned['pos'] == i].alt.item():
+            #  minor is alt
+            #  Find if minor is 1 or 0 in phase
+            if df['phase'].sum() > (df.shape[0] / 2):
+                alt = 0
+                ref = 1
         else:
-            minor = minor_df[anc].sum()
-        if isinstance(minor, str):
+            #  major is alt
+            if df['phase'].sum() > (df.shape[0] / 2):
+                alt = 1
+                ref = 0
+        alt_df = df.loc[df['phase'] == alt]
+        if alt_df.empty:
+            alt_sum = 0
+        else:
+            alt_sum = alt_df[anc].sum()
+        if isinstance(alt_sum, str):
             print("Minor.dtype is string for " + str(i) + str(anc) + str(chrom))
             skipped_snps += 1
             continue
-        major_df = df.loc[df['phase'] == 1]
-        if major_df.empty:
-            major = 0
+        ref_df = df.loc[df['phase'] == ref]
+        if ref_df.empty:
+            ref_sum = 0
         else:
-            major = major_df[anc].sum()  # Sum painting prob for major allele
-        if isinstance(major, str):
+            ref_sum = ref_df[anc].sum()  # Sum painting prob for major allele
+        if isinstance(ref_sum, str):
             print("Major.dtype is string for " + str(i) + str(anc) + str(chrom))
             skipped_snps += 1
             continue
-        if minor + major != 0:
-            anc_maf = minor/(minor+major)
+        if alt_sum + ref_sum != 0:
+            anc_maf = alt_sum / (alt_sum + ref_sum)
         else:
             anc_maf = 0
         if GWAS_pruned.loc[GWAS_pruned['pos'] == i].empty:
@@ -89,7 +104,8 @@ idfile = pd.read_csv(args.idfile, header=None, sep=" ", index_col=0)
 # idfile = idfile[~idfile.index.isin(idfile.tail(318).index.tolist())]
 
 # Read in phasefile
-positions = anc_copyprobs.columns.tolist()[::-1]
+# positions = anc_copyprobs.columns.tolist()[::-1]  # Is this re-mislabelling? Pretty sure yes
+positions = anc_copyprobs.columns.tolist()
 phase = pd.read_csv(str(args.phasefile), header=None, sep=" ", dtype='int8')
 phase.columns = positions
 phase.index = [val for val in idfile.index.unique().tolist() for _ in (0, 1)]
@@ -102,17 +118,31 @@ results_list = list()
 if not os.path.exists("PRS_calculations"):
     open("PRS_calculations", 'a').close()
 
+# Load variants file to find which is alt allele (not always minor!)
+variants = pd.read_csv("/willerslev/datasets/UKBiobank/NealeV2/variants.tsv.gz",
+                       sep='\t',
+                       usecols=['variant', 'chr', 'alt'],
+                       dtype={'variant': 'string', 'chr': 'string', 'alt': 'string'})
+variants = variants.loc[variants['chr'] == str(args.chr)]
+alt = 1
+ref = 0
 # LOOP through GWAS files
 phenotypes = pd.read_csv(args.phenotypes, header=None, dtype=str)[0].tolist()
 phenotypes = [s.strip() for s in phenotypes]
 for file in phenotypes:
-    GWAS = pd.read_csv(file, sep='\t', usecols=['beta', 'pval', 'variant'])
+    GWAS = pd.read_csv(file,
+                       sep='\t',
+                       usecols=['variant', 'beta', 'pval', 'minor_allele'],
+                       dtype={'variant': 'string', 'beta': 'float', 'pval': 'float', 'minor_allele': 'string'})
     print("***Looking at " + str(file) + "***")
     GWAS[['chr', 'pos', '1', '2']] = GWAS['variant'].str.split(':', expand=True)
     GWAS = GWAS[GWAS['chr'] == str(args.chr)]
     GWAS = GWAS.loc[GWAS['pos'].isin(positions)]  # NB about 80% of painted SNPs are in GWAS file
+
+    GWAS_variants = pd.merge(variants[['variant', 'alt']], GWAS, on='variant')
+
     for p in [0.01, 0.05, 0.1]:
-        GWAS_pval = GWAS[GWAS['pval'] <= p]
+        GWAS_pval = GWAS_variants[GWAS_variants['pval'] <= p]
         for LD in ["05.merged.filtered.prune.in", "5.merged.filtered.prune.in", "9.merged.filtered.prune.in"]:
             pruned_in = pd.read_csv(str(args.pruned_dir) + "/" + str(args.chr) + "." + str(LD), header=None)
             GWAS_pruned = GWAS_pval.loc[GWAS_pval['pos'].isin(pruned_in[0].astype(str).tolist())]
@@ -125,23 +155,3 @@ print("***Success! Now writing results to output file***")
 PRS_calculations = pd.DataFrame.from_records(results_list)
 # PRS_calculations = pd.DataFrame([anc_dict])
 PRS_calculations.to_csv('PRS_calculations', mode='a', header=False, index=False, sep=" ")
-
-# PRS_dict = dict()
-# for anc in ["CHG", "EHG", "Farmer", "African", "EastAsian", "WHG", "Yamnaya"]:
-#     PRS_per_anc = 0
-#     total_number_of_SNPs = 0
-#     for chrom, PRS, number_of_SNPs in anc_dict[anc]:
-#         PRS_per_anc += PRS
-#         total_number_of_SNPs += number_of_SNPs
-#     PRS_dict[anc] = PRS_per_anc
-#     PRS_dict[anc + "_perSNP"] = PRS_per_anc/total_number_of_SNPs
-# PRS_dict['Phenotype'] = args.file_name
-# PRS_dict['LD_pruned'] = args.pruned_in
-# PRS_dict['p_value'] = args.p_value
-# PRS_calculations_temp = pd.DataFrame([PRS_dict])
-# PRS_calculations_temp = PRS_calculations_temp[["Phenotype", "LD_pruned", "p_value", "CHG", "EHG", "Farmer",
-#                                                "African", "EastAsian", "WHG", "Yamnaya", "CHG_perSNP", "EHG_perSNP",
-#                                                "Farmer_perSNP", "African_perSNP", "EastAsian_perSNP", "WHG_perSNP",
-#                                                "Yamnaya_perSNP"]]
-# PRS_calculations_temp.to_csv('PRS_calculations', mode='a', index=False, header=True, sep=" ")
-# print("*** Success! See PRS_calculations for results ***")

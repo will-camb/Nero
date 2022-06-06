435af5cfd48050a83589777459952772baec06e9
will-camb <williamfsbarrie@gmail.com>
2022-06-06 11:29:02 +0100

    Updated imputation scripsts including one by one for parallelisation

--- scripts/impute_ancestry_bp.py
+++ scripts/impute_ancestry_bp.py
@@ -9,8 +9,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-copyprobs_file",
                     help="Should be named in form anc.chr.master_all_copyprobsperlocus.txt.gz",
                     required=True)
-parser.add_argument("-snp_file",
-                    help="File containing details of SNPs to be imputed. Should be two columns: chr and position",
+parser.add_argument("-bp",
                     required=True)
 parser.add_argument("-chr",
                     help="Chromosome number",
@@ -27,52 +26,8 @@ if args.reverse_cols == 'True':
 else:
     reverse_cols = False
 
-if reverse_cols:  # For copyprobs files with incorrectly ordered columns
-    def impute(impute_snp_list_):
-        global painted_snp_list
-        df = pd.DataFrame(index=anc_copyprobs.index)
-        for snp in impute_snp_list_:
-            if snp in painted_snp_list:
-                df.loc[:, snp] = anc_copyprobs[str(snp)]
-            else:
-                painted_snp_list.append(snp)
-                painted_snp_list.sort(reverse=True)
-                temp_anc_copyprobs = anc_copyprobs.iloc[:, [painted_snp_list.index(snp) - 1, painted_snp_list.index(snp)]]
-                D1 = abs(snp - temp_anc_copyprobs.columns.astype(int).tolist()[0])
-                D2 = abs(snp - temp_anc_copyprobs.columns.astype(int).tolist()[1])
-                temp_anc_copyprobs.loc[:, snp] = (1 - (D1 / (D1 + D2))) * temp_anc_copyprobs.iloc[:, 0] + (
-                        1 - (D2 / (D1 + D2))) * temp_anc_copyprobs.iloc[:, 1]
-                df.loc[:, snp] = temp_anc_copyprobs[snp]
-        return df
-
-
-    Path("imputed/").mkdir(parents=True, exist_ok=True)
-    if Path("imputed/temp." + str(args.anc) + "." + str(args.chr) + ".master_all_copyprobsperlocus.txt.gz").is_file():
-        print("temp." + str(args.anc) + "." + str(args.chr) + ".master_all_copyprobsperlocus.txt.gz already imputed")
-        sys.exit()
-    col_names = pd.read_csv(str(args.copyprobs_file), sep=" ", nrows=0).columns
-    types_dict = {'0': str}
-    types_dict.update({col: 'int8' for col in col_names if col not in types_dict})
-    anc_copyprobs = pd.read_csv(str(args.copyprobs_file), sep=" ", dtype=types_dict)
-    anc_copyprobs.set_index("0", inplace=True)
-    anc_copyprobs.columns = anc_copyprobs.columns.tolist()[::-1]  # Reverse column names because they are mis-labelled;
-    #  so when correct, column names are descending
-    painted_snp_list = anc_copyprobs.columns.astype(int).tolist()
-    impute_snps = pd.read_csv(args.snp_file)
-    impute_snps = impute_snps.loc[impute_snps.iloc[:, 0] == int(args.chr)]
-    impute_snp_list = impute_snps.iloc[:, 1].tolist()
-    impute_snp_list = list(set(impute_snp_list))
-    impute_snp_list = [int(x) for x in impute_snp_list]
-    if len(impute_snp_list) == 0:
-        print(f"No SNPs to be imputed for {args.chr} exiting")
-        sys.exit()
-    impute_snp_list.sort(reverse=True)
-    imputed_snps = impute(impute_snp_list)
-    imputed_snps.columns = imputed_snps.columns.tolist()[::-1]
-    # Reverse column names to incorrect order for other scripts
-    imputed_snps.to_csv("imputed/temp." + str(args.anc) + "." + str(args.chr) + ".master_all_copyprobsperlocus.txt.gz",
-                        compression='gzip')
-
+if reverse_cols:  # Can't run on incorrectly ordered cols
+    sys.exit()
 else:  # For correctly labelled cols in copyprobs:
     def impute(impute_snp_list_):
         global painted_snp_list
@@ -110,15 +65,12 @@ else:  # For correctly labelled cols in copyprobs:
     painted_snp_list = pd.read_csv(str(args.copyprobs_file), sep=" ", nrows=0).columns.tolist()  # descending=correct
     del painted_snp_list[0]
     painted_snp_list = [int(x) for x in painted_snp_list]
-    impute_snps = pd.read_csv(args.snp_file)
-    impute_snps = impute_snps.loc[impute_snps.iloc[:, 0] == int(args.chr)]
-    impute_snp_list = impute_snps.iloc[:, 1].tolist()
-    impute_snp_list = list(set(impute_snp_list))
+    impute_snp_list = [args.bp]
     impute_snp_list = [int(x) for x in impute_snp_list]
     if len(impute_snp_list) == 0:
         print("No SNPs to be imputed for " + str(args.chr) + ", exiting")
         sys.exit()
     impute_snp_list.sort(reverse=True)  # descending
     imputed_snps = impute(impute_snp_list)  # NB saves in correct order!
-    imputed_snps.to_csv("imputed/temp." + str(args.anc) + "." + str(args.chr) + ".master_all_copyprobsperlocus.txt.gz",
+    imputed_snps.to_csv("imputed/temp." + str(args.bp) + "." + str(args.anc) + ".master_all_copyprobsperlocus.txt.gz",
                         compression='gzip')
